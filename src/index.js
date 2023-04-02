4091733631c76eda946ff4492af882315d56d763
denis-tsibaev <denis.tsibaev@gmail.com>
2023-04-02 18:39:30 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,56 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '22318307-8fc961fa8d00a621cd6d86864';
+const queryStringOptions =
+  'image_type=photo&orientation=horizontal&safesearch=true';
+import galleryMarkup from './templates/cards-template.js';
+
+// const options = {
+//   webformatURL,
+//   largeImageURL,
+//   tags,
+//   likes,
+//   views,
+//   comments,
+//   downloads,
+// };
+
+async function fetchApi(query) {
+  const response = await fetch(
+    `${BASE_URL}?key=${API_KEY}&q=${query}&${queryStringOptions}`
+  );
+  const data = await response.json();
+  console.log('data: ', data);
+  return data;
+}
+
 const formEl = document.querySelector('#search-form');
+const galleyEl = document.querySelector('.js-gallery');
 
 formEl.addEventListener('submit', inputHandler);
 
 function inputHandler(event) {
   event.preventDefault();
   const searchQuery = event.target.searchQuery.value.trim();
+  try {
+    fetchApi(searchQuery).then(render);
+    console.log('searchQuery: ', searchQuery);
+  } catch (error) {
+    console.log('error message in try-catch: ', error.message);
+  }
+}
 
-  console.log('searchQuery: ', searchQuery);
+function render(data) {
+  galleyEl.innerHTML = galleryMarkup(data);
+  lightbox();
+}
+
+function lightbox() {
+  new SimpleLightbox('.photo-card a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    overlayOpacity: 0.8,
+  });
 }
--- src/templates/cards-template.js
+++ src/templates/cards-template.js
@@ -1,17 +1,39 @@
-<div class="photo-card">
-  <img src="" alt="" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-    </p>
-  </div>
-</div>;
+export default function galleryMarkup(data) {
+  const obj = data.hits;
+  const cardsByString = obj
+    .map(item => {
+      const {
+        tags,
+        largeImageURL,
+        webformatURL,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = item;
+      return `
+	<div class="photo-card">
+    	<a href="${largeImageURL}">
+		<img src="${webformatURL}" alt="${tags}" loading="lazy" />
+		</a>
+		<div class="info">
+      		<p class="info-item">
+        	<b>Likes</b>${likes}
+      		</p>
+      		<p class="info-item">
+        	<b>Views</b>${views}
+      		</p>
+      		<p class="info-item">
+        	<b>Comments</b>${comments}
+      		</p>
+      		<p class="info-item">
+        	<b>Downloads</b>${downloads}
+      		</p>
+    	</div>
+	</div>
+	`;
+    })
+    .join('');
+
+  return cardsByString;
+}
